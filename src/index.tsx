0314a7e104cd8de5ef19f33c4d4a85e64734bfcd
Argayash <zilberman.yakoff@yandex.ru>
2024-07-10 14:19:23 +0300

    fix: дописал правило в конфиге es-lint для того, чтобы он работал

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
-import style from './index.module.scss';
-import './index.scss';
-import './assets/images/file.jpg';
+import style from './index.module.scss'
+import './index.scss'
+import './assets/images/file.jpg'
 
-const numbers: number[] = [2, 3, 5];
+const numbers: number[] = [2, 3, 5]
 
 // Стрелочная функция. Не запнётся ли на ней Internet Explorer?
-const doubledNumbers = numbers.map((number) => number * 2);
+const doubledNumbers = numbers.map((number) => number * 2)
 
-console.log(doubledNumbers); // 4, 6, 10
-console.log(style); //использование объекта style важно для попадания стилей в css-бандл
+console.log(doubledNumbers) // 4, 6, 10
+console.log(style) //использование объекта style важно для попадания стилей в css-бандл
