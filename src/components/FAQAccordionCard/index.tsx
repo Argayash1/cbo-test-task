01301b74ad00a98a183fe37c25393f3d9ef2147d
Argayash <zilberman.yakoff@yandex.ru>
2024-07-19 18:12:27 +0300

    feat: сделал вёрстка FAQ для планшета

--- src/components/FAQAccordionCard/index.tsx
+++ src/components/FAQAccordionCard/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from './FAQAccordionCard.module.scss'
 import clsx from 'clsx'
+import useResize from '../../hooks/useResize'
 
 interface FAQAccordionCardProps {
 	question: string
@@ -15,12 +16,28 @@ export const FAQAccordionCard = ({
 	onClick,
 	isOpen,
 }: FAQAccordionCardProps) => {
+	const screenWidth = useResize()
+
 	const answerRef = React.useRef<HTMLParagraphElement>(null)
+
+	const answerOnTablet =
+		'Волшебной таблетки, которая точно поможет выжить каждому бизнесу, не будет. Мы разберем множество разных инструментов и изучим, как их применять. Комбинировать и подстраивать эти инструменты под свой бизнес нужно будет самим на практике.'
 
 	return (
 		<article className={styles.root}>
-			<button className={styles.questionButton} onClick={onClick}>
-				{question}
+			<button
+				className={clsx(styles.questionButton, {
+					[styles.questionButtonOpenInTablet]: isOpen && screenWidth <= 986,
+				})}
+				onClick={onClick}
+			>
+				<span
+					className={clsx(styles.question, {
+						[styles.questionOpenInTablet]: isOpen && screenWidth <= 986,
+					})}
+				>
+					{question}
+				</span>
 				<span
 					className={clsx(styles.buttonIcon, {
 						[styles.buttonIconRotated]: isOpen,
@@ -36,7 +53,9 @@ export const FAQAccordionCard = ({
 				}}
 			>
 				<p className={styles.answer} ref={answerRef}>
-					{answer}
+					{screenWidth <= 986 && answer.startsWith('Текущие')
+						? answerOnTablet
+						: answer}
 				</p>
 			</div>
 		</article>
